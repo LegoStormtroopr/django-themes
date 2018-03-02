da9625e61325cb83a3036d50633ea53e23d2869b
Harry White <harry.white424@gmail.com>
2018-03-02 11:30:19 +1100

    Quick fix for database_files loading

--- django_themes/storage.py
+++ django_themes/storage.py
@@ -2,14 +2,26 @@ from django.conf import settings
 from django.utils.functional import LazyObject
 from django.utils.module_loading import import_string
 
+from django.core.files.storage import FileSystemStorage
+
+try:
+    from database_files.storage import DatabaseStorage
+except:
+    DatabaseStorage = None
 
 def get_storage_class(import_path=None):
-    return import_string(import_path or settings.THEMES_FILE_STORAGE)
+    module = import_path or settings.THEMES_FILE_STORAGE
 
+    if module == 'dbstorage' and DatabaseStorage:
+        return DatabaseStorage()
+    else:
+        return import_string(module)
 
-class DefaultStorage(LazyObject):
+class DefaultFileStorage(LazyObject):
     def _setup(self):
-        self._wrapped = get_storage_class()(location=settings.THEMES_FILE_ROOT)
+        self._wrapped = get_storage_class(location=settings.THEMES_FILE_ROOT)
 
-
-default_theme_storage = DefaultStorage()
+if isinstance(get_storage_class(), FileSystemStorage):
+    default_theme_storage = DefaultFileStorage()
+else:
+    default_theme_storage = get_storage_class()
