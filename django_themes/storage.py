92c71f978b25b83c49e376156895508dccf93d38
Harry White <harry.white424@gmail.com>
2018-03-02 11:40:58 +1100

    Properly fixed storage loading

--- django_themes/storage.py
+++ django_themes/storage.py
@@ -4,24 +4,21 @@ from django.utils.module_loading import import_string
 
 from django.core.files.storage import FileSystemStorage
 
-try:
-    from database_files.storage import DatabaseStorage
-except:
-    DatabaseStorage = None
-
 def get_storage_class(import_path=None):
     module = import_path or settings.THEMES_FILE_STORAGE
-
-    if module == 'dbstorage' and DatabaseStorage:
-        return DatabaseStorage()
-    else:
-        return import_string(module)
+    return import_string(module)
 
 class DefaultFileStorage(LazyObject):
     def _setup(self):
-        self._wrapped = get_storage_class(location=settings.THEMES_FILE_ROOT)
+        self._wrapped = get_storage_class()(location=settings.THEMES_FILE_ROOT)
 
-if isinstance(get_storage_class(), FileSystemStorage):
+class DefaultStorage(LazyObject):
+    def _setup(self):
+        self._wrapped = get_storage_class()()
+
+storage_class = get_storage_class()
+
+if issubclass(storage_class, FileSystemStorage):
     default_theme_storage = DefaultFileStorage()
 else:
-    default_theme_storage = get_storage_class()
+    default_theme_storage = DefaultStorage()
