e3ef31f0321a487f12f0fb783b4cc1285f79b792
Harry White <harry.white424@gmail.com>
2018-03-05 09:19:43 +1100

    Simplified setting storage class

--- django_themes/storage.py
+++ django_themes/storage.py
@@ -8,13 +8,9 @@ def get_storage_class(import_path=None):
     module = import_path or settings.THEMES_FILE_STORAGE
     return import_string(module)
 
-class DefaultFileStorage(LazyObject):
-    def _setup(self):
-        self._wrapped = get_storage_class()(location=settings.THEMES_FILE_ROOT)
-
 storage_class = get_storage_class()
 
 if issubclass(storage_class, FileSystemStorage):
-    default_theme_storage = DefaultFileStorage()
+    default_theme_storage = storage_class(location=settings.THEMES_FILE_ROOT)
 else:
     default_theme_storage = storage_class()
